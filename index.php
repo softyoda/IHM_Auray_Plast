a49cf3ed746f0ab0ee657eee089892cd21731492
softyoda <yo561@live.fr>
2017-06-09 02:56:06 +0200

    Index.php
    Ajout de l'index.php

---
+++ index.php
@@ -0,0 +1,147 @@
+<html><head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+        <script type="text/javascript" src="http://netdna.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+        <link href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+        <link href="http://pingendo.github.io/pingendo-bootstrap/themes/default/bootstrap.css" rel="stylesheet" type="text/css">
+    </head>
+	<style>
+	#footer { 
+		background-color: #303030 !important
+
+	}
+	</style>
+	<body>
+
+<div class="navbar navbar-default navbar-inverse navbar-static-top">
+<div class="container">
+<div class="navbar-header">
+<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-ex-collapse">
+<span class="sr-only">Toggle navigation</span><span class="icon-bar">
+</span>
+<span class="icon-bar">
+</span><span class="icon-bar">
+</span></button>
+<a class="navbar-brand" href="#">
+    <span>Aurayplast</span>
+</a>
+</div>
+<div class="collapse navbar-collapse" id="navbar-ex-collapse">
+    <ul class="nav navbar-nav navbar-right">
+        <li>
+            <a href="signin.php">Connexion</a>
+        </li>
+        <li>
+			<a href="register.php">S'enregistrer</a>
+        </li>
+    </ul>
+</div>
+</div>
+</div>
+<div class="section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <h1>Aurayplast</h1>
+                <h3>Présentation</h3>
+                <p>
+					Spécialisée depuis 1974 dans la production de joints d'étanchéité en POLYURETHANE 
+					et de pièces techniques en matières plastiques injecté, AURAY PLAST vous apporte son
+					expérience et un service complet dans l'étude, la conception et la réalisation de vos
+					composants mécaniques plastiques.
+				</p></br>
+				
+				<p>
+					Le but de notre projet et de metre en place différents capteurs et actionneurs autour de la machine a injection.
+					Les données de ces capteurs sont stocker dans une base de donnée pour etre visualiser a distance via ce site.
+					Les capteurs servent a mesurer différentes données tels que la température, la pression, le taux d'humidité.
+				</p>
+				
+				<p>
+					Pour cela nous nous sommes séparer les taches 
+				</p>
+            </div>
+            <div class="col-md-6"> <img src="http://pingendo.github.io/pingendo-bootstrap/assets/placeholder.png" class="img-responsive"> </div>
+        </div>
+    </div>
+</div>
+<div class="section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4"> <img src="http://pingendo.github.io/pingendo-bootstrap/assets/placeholder.png" class="img-responsive">
+                <center><h2>Panneau d'administration des machines a injections</h2></center>
+                <p>
+				<p>
+					Plan du site : </br>
+					<a href="/administration/signin.php">- Connexion</a></br>
+					<a href="/administration/register.php">- S'enregistrer</a></br>
+					<a href="/administration/home.php">- Acceuil</a></br>
+					<a href="/administration/add-engine.php">- Ajouter une machine a injection</a></br>
+					<a href="/administration/user.php">- Panneau d'administration de l'utilisateur</a></br>
+					<a href="/administration/assets">- Dossier comportant les assets</a></br>
+				</p>
+				<p>
+					
+				</p>
+            </div>
+            <div class="col-md-4"> <img src="http://pingendo.github.io/pingendo-bootstrap/assets/placeholder.png" class="img-responsive">
+                <center><h2>Pages de génération du fichier scripte</h2></center>
+				<p>
+					Plan du site : </br>
+					<a href="/script/phpBDauray01a.php">- Configuration centrale</a></br>
+					<a href="/script/phpBDauray02a.php">- Generation script</a></br>
+					<a href="/script/phpBDauray03a.php">- Ecriture script</a></br>
+					<a href="/script/phpFTPauray01a.php">- Transmission script</a></br>
+					
+				</p>
+                <p>
+					Constituer un fichier Script listant les diverses mesures à effectuer,
+					les divers actions à effectuer et les mesures à transmettre à la Base de Donnée vers le Serveur de Mesures</br>
+				</p>
+            </div>
+            <div class="col-md-4"> <img src="http://pingendo.github.io/pingendo-bootstrap/assets/placeholder.png" class="img-responsive">
+                <center><h2>Autres documents</h2></center>
+                <p>
+					</br>
+					<a href="assets/pdf/projet.pdf" target="_blank">Affiche de présentation</a></br></br>
+					<a href="assets/img/Affiche-presentation.png" target="_blank">Dossier technique</a></br>
+				</p>
+				<p>
+					Dossier technique comprenant une partie commune au projet chaques parties individuelles
+				</p>
+            </div>
+        </div>
+    </div>
+</div>
+<footer class="hidden-xs section section-primary" id="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6">
+                <h1>Contact</h1>
+                <p>&nbsp;<b>Adresse :</b> 5 rue Yves Kerguelen BP 40212 - 56402 Auray&nbsp;
+                    <br>
+                    <br><b>Téléphone : </b>33 (0) 2 97 24 21 63
+                    <br>
+                    <br>&nbsp;<b>Email : </b>contact@aurayplast.fr&nbsp;</p>
+            </div>
+            <div class="col-sm-6">
+                <p class="text-info text-right">
+                    <br>
+                    <br>
+                </p>
+                <div class="row">
+                    <div class="col-md-12 hidden-lg hidden-md hidden-sm text-left">
+					<a href="#"><i class="fa fa-3x fa-fw fa-instagram text-inverse"></i></a>
+					<a href="#"><i class="fa fa-3x fa-fw fa-twitter text-inverse"></i></a> 
+					<a href="#"><i class="fa fa-3x fa-fw fa-facebook text-inverse"></i></a> 
+					<a href="#"><i class="fa fa-3x fa-fw fa-github text-inverse"></i></a> 
+					</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+</body>
+
+</html>
